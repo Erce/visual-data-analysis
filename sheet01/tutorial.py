3c21850c71b7059eba85a874a1f3b9507584f901
Rechargeable22 <oliver.leuschner@gmx.de>
2020-04-24 16:31:22 +0200

    tasks b,c

--- sheet01/tutorial.py
+++ sheet01/tutorial.py
@@ -6,7 +6,15 @@ import seaborn as sns
 sns.set()
 tips = sns.load_dataset("tips")
 # ['total_bill', 'tip', 'sex', 'smoker', 'day', 'time', 'size']
+
+# a
 # We can see that there is no data on for lunch meals on sunday and saturday
-sns.relplot(data=tips, x="day", y="time")
+# sns.catplot(data=tips, x="day", y="total_bill", col="time", hue="day")
+
+# b
+# sns.relplot(data=tips, x="total_bill", y="tip", col="sex", hue="sex")
+
+# c
+sns.relplot(data=tips, x="tip", y="total_bill")
 
 plt.show()
